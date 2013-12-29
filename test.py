a43f0feceeda89fa474fe7341dffd85017d4501f
kmhawkes <katiemhawkes@gmail.com>
2013-12-28 21:51:31 -0800

    use multiple offsets

--- test.py
+++ test.py
@@ -11,15 +11,21 @@ token_secret    = 'wZPFX56O173NJNTsZ12GS91W3OQ'
 # search arguments
 search_location = 'san diego'
 search_term     = 'mexican restaurant'
+search_offsets  = [0, 20, 40]
 
 # initialize an api object
 yelp_api = YelpAPI(consumer_key, consumer_secret, token, token_secret)
 
 # look here for the terms you can use for searching and their meaning
 # http://www.yelp.com/developers/documentation/v2/search_api#searchMSL
-search_results = yelp_api.search_query(
-                     location = search_location,
-                     term     = search_term,
-                 )
-for b in search_results.get('businesses'):
-    pprint(b)
+for offset in search_offsets:
+    print "At offset: %d" % offset
+    search_results = yelp_api.search_query(
+                         location = search_location,
+                         term     = search_term,
+                         offset   = offset,
+                     )
+
+    for biz in search_results.get('businesses'):
+        #pprint(b)
+        print '%s - %s - %s' % ( biz.get('name'), biz.get('rating'), biz.get('location').get('cross_streets') )
